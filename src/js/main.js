996da1a0d544b75259010fd30482e72f7dddcdb1
alex <admin@createsites.net>
2023-07-08 00:13:30 -0300

    Added logo, updated photo, some fixes.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 import { Fancybox } from "@fancyapps/ui";
 
+// fancy doc https://fancyapps.com/fancybox/captions/
+// swiper doc https://swiperjs.com/
+// consider to make a carousel with fancy apps
+
 Fancybox.bind('[data-fancybox="diplomas"]', {});
 Fancybox.bind('[data-fancybox="certificates"]', {});
 
